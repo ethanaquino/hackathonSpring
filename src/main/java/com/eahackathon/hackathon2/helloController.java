e9433d9f451099c390b0a0661ff907163d9a3190
Pivotal <eaq1499@gmail.com>
2019-06-14 11:58:55 -0400

    implemented autowired instead of new method call for best coding practices

--- src/main/java/com/eahackathon/hackathon2/helloController.java
+++ src/main/java/com/eahackathon/hackathon2/helloController.java
@@ -1,5 +1,6 @@
 package com.eahackathon.hackathon2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class helloController {
 
+    @Autowired
+    helloService helloService;
+
         @RequestMapping("/hello")
         public ResponseEntity<String> getHello() {
-            return ResponseEntity.ok(new helloService().hello());
+            return ResponseEntity.ok(helloService.hello());
         }
 }
